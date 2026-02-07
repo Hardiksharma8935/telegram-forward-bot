870140bb35f76ea98c810e726dfd2a88f0b8256a
Hardiksharma8935 <hardikrampur440@gmail.com>
2026-02-07 17:02:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8564077935:AAE1bPGBr_kmIpEx3n8TTcIWyCypYEiHgK4")
 
-SOURCE_CHAT_ID = 0   # baad me bharenge
-DEST_CHAT_ID = 0     # baad me bharenge
+SOURCE_CHAT_ID = -5146530739    # baad me bharenge
+DEST_CHAT_ID = -5140519863    # baad me bharenge
 
 async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat.id == SOURCE_CHAT_ID:
